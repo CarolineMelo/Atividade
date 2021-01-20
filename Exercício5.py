26bedbb7ac294a8aa952e38a40c153533cb0a29b
Caroline <2015302@aluno.univesp.br>
2021-01-19 22:17:00 -0300

    Exercícios

--- Exercício5.py
+++ Exercício5.py
@@ -4,4 +4,4 @@ n2 = float ( input ("Segunda nota do aluno: "))
 n3 = float ( input ( "Terceira nota do aluno:"))
 n4 = float ( input ( "Quarta nota do aluno:"))
 média = (n1 + n2 + n3 + n4) / 4
-print("O aluno" , nome, "teve notas", n1,",",n2,"," n3, "e" n4, "e sua média é:", média)
+print("O aluno" ,nome, "teve notas", n1,",",n2,"," ,n3, "e" ,n4, "e sua média é:" ,média)
