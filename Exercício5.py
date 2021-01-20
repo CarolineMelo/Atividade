3637d4132a662f92117ae480801dc928c75ddfd3
Caroline <2015302@aluno.univesp.br>
2021-01-19 22:08:23 -0300

    Exercícios

---
+++ Exercício5.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome:")
+n1 = float ( input ("Primeira nota do aluno: "))
+n2 = float ( input ("Segunda nota do aluno: "))
+n3 = float ( input ( "Terceira nota do aluno:"))
+n4 = float ( input ( "Quarta nota do aluno:"))
+média = (n1 + n2 + n3 + n4) / 4
+print("O aluno" , nome, "teve notas", n1,",",n2,"," n3, "e" n4, "e sua média é:", média)
